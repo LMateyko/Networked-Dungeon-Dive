f0414d2b91cae686225983bafa95c8a5c22bce4c
RogueLink <LMateyko@gmail.com>
2025-09-16 21:02:28 -0700

    Basic Enemy
    - Adding First pass of enemies
    - Creating their onw ability system components and attribute sets
    - Initial comments to show attached UI
    - Added paragon minion data from Fab. Files too large, need to ignore.

--- Source/Networked_RPG/Private/NDDEnemy.cpp
+++ Source/Networked_RPG/Private/NDDEnemy.cpp
@@ -1,5 +1,79 @@
 // Copyright 2025 Lucas Mateyko
 
-
 #include "NDDEnemy.h"
 
+#include "NDDAbilitySystemComponent.h"
+#include "NDDAttributeSetBase.h"
+
+ANDDEnemy::ANDDEnemy()
+{
+	// Create ability system component, and set it to be explicitly replicated
+	EnemyAbilitySystemComponent = CreateDefaultSubobject<UNDDAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	EnemyAbilitySystemComponent->SetIsReplicated(true);
+
+	// Minimal Mode means that no GameplayEffects will replicate. They will only live on the Server. Attributes, GameplayTags, and GameplayCues will still replicate to us.
+	EnemyAbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+	// Set our parent's TWeakObjectPtr
+	AbilitySystemComponent = EnemyAbilitySystemComponent;
+
+	// Create the attribute set, this replicates by default
+	// Adding it as a subobject of the owning actor of an AbilitySystemComponent
+	// automatically registers the AttributeSet with the AbilitySystemComponent
+	EnemyAttributeSetBase = CreateDefaultSubobject<UNDDAttributeSetBase>(TEXT("AttributeSetBase"));
+
+	// Set our parent's TWeakObjectPtr
+	AttributeSetBase = EnemyAttributeSetBase;
+}
+
+void ANDDEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent.IsValid())
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this);
+		InitializeAttributes();
+		AddCharacterAbilities();
+
+		//// Setup FloatingStatusBar UI for Locally Owned Players only, not AI or the server's copy of the PlayerControllers
+		//APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+		//if (PC && PC->IsLocalPlayerController())
+		//{
+		//	if (UIFloatingStatusBarClass)
+		//	{
+		//		UIFloatingStatusBar = CreateWidget<UGDFloatingStatusBarWidget>(PC, UIFloatingStatusBarClass);
+		//		if (UIFloatingStatusBar && UIFloatingStatusBarComponent)
+		//		{
+		//			UIFloatingStatusBarComponent->SetWidget(UIFloatingStatusBar);
+
+		//			// Setup the floating status bar
+		//			UIFloatingStatusBar->SetHealthPercentage(GetHealth() / GetMaxHealth());
+
+		//			UIFloatingStatusBar->SetCharacterName(CharacterName);
+		//		}
+		//	}
+		//}
+
+		// Attribute change callbacks
+		HealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetHealthAttribute()).AddUObject(this, &ANDDEnemy::HealthChanged);
+
+	}
+}
+
+void ANDDEnemy::HealthChanged(const FOnAttributeChangeData& Data)
+{
+	float Health = Data.NewValue;
+
+	//// Update floating status bar
+	//if (UIFloatingStatusBar)
+	//{
+	//	UIFloatingStatusBar->SetHealthPercentage(Health / GetMaxHealth());
+	//}
+
+	//// If the minion died, handle death
+	//if (!IsAlive() && !AbilitySystemComponent->HasMatchingGameplayTag(DeadTag))
+	//{
+	//	Die();
+	//}
+}
--- Source/Networked_RPG/Public/NDDEnemy.h
+++ Source/Networked_RPG/Public/NDDEnemy.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "NDDCharacter.h"
+
 #include "NDDEnemy.generated.h"
 
 /**
@@ -14,4 +15,31 @@ class NETWORKED_RPG_API ANDDEnemy : public ANDDCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	ANDDEnemy();
+
+protected:
+	// Actual hard pointer to AbilitySystemComponent
+	UPROPERTY()
+	TObjectPtr<class UNDDAbilitySystemComponent> EnemyAbilitySystemComponent;
+
+	// Actual hard pointer to AttributeSetBase
+	UPROPERTY()
+	TObjectPtr<class UNDDAttributeSetBase> EnemyAttributeSetBase;
+
+	//UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "UI")
+	//TSubclassOf<class UNDDFloatingStatusBarWidget> UIFloatingStatusBarClass;
+
+	//UPROPERTY()
+	//class UNDDFloatingStatusBarWidget* UIFloatingStatusBar;
+
+	//UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "UI")
+	//class UWidgetComponent* UIFloatingStatusBarComponent;
+
+	virtual void BeginPlay() override;
+
+	FDelegateHandle HealthChangedDelegateHandle;
+
+	// Attribute changed callbacks
+	virtual void HealthChanged(const FOnAttributeChangeData& Data);
 };
