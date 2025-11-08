66381d9cec0ae6111341b5f08d115f10da1697d1
RogueLink <LMateyko@gmail.com>
2025-11-08 09:50:16 -0800

    Basic Attack Damage Update
    - Migrate Damage and Gameplay effect variables from blueprint to code class
    - Adding StunValue for future stun implementation.

--- Source/Networked_RPG/Public/NDDGA_BasicAttack.h
+++ Source/Networked_RPG/Public/NDDGA_BasicAttack.h
@@ -13,5 +13,19 @@ UCLASS()
 class NETWORKED_RPG_API UNDDGA_BasicAttack : public UNDDGameplayAbility
 {
 	GENERATED_BODY()
+
+protected:
+
+	// Health Damage applied to the target
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Damage")
+	float DamageValue;
+
+	// Health Damage applied to the target
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Damage")
+	float StunValue;
+
+	// Gameplay Effect for applying damage to the target
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Damage")
+	TSubclassOf<UGameplayEffect> DamageGameplayEfect;
 	
 };
